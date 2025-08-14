5351136fd2c8d2149d3a744aa1aa2f833dfbc59f
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-08-14 21:51:43 +0530

    created the base employee dashboard

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const SubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("EMAIL IS", email);
+    console.log("Password IS", password);
+
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="border-2 rounded-xl border-emerald-600 p-20">
+        <form
+          onSubmit={(e) => {
+            SubmitHandler(e);
+          }}
+          className="flex flex-col items-center justify-center"
+        >
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              // console.log(e.target.value);
+            }}
+            required
+            className="outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400"
+            type="email"
+            placeholder="Enter your Email"
+          />
+
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+            className="outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full mt-3 placeholder:text-gray-400"
+            type="password"
+            placeholder="Enter Password"
+          />
+
+          <button className="mt-7 text-white border-none outline-none border-2 bg-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-white">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
